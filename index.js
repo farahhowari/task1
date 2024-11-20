f82889e296b04ae7af18036364f718af509efa2d
farahhowari <farahhowari783@gmail.com>
2024-11-21 02:50:03 +0300

    functhion

--- index.js
+++ index.js
@@ -64,3 +64,14 @@ findp [i].innerHTML=" this is an update form for loop"
 
 }
 }
+function sumation (){
+   var input1=document.getElementById('input1').value
+   var input2=document.getElementById('input2').value
+   var z= parseInt(input1)+parseInt(input2)
+
+   var result = document.getElementById('result')
+   result.innerHTML=z
+}
+
+
+
